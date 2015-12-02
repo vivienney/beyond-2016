7a3696579cb6c92577a2cde147a81b181bb77735
Sam Mason <sam.jbmason@gmail.com>
2015-12-02 12:55:12 +0000

    Update 2015-12-02T12:55:09.120Z

--- src/js/main.js
+++
@@ -1,9 +0,0 @@
-import $ from 'jquery';
-import mailchimp from './mailchimp';
-
-var form = $('.news-signup__form');
-
-form.on('submit', function (event) {
-  mailchimp.register(form);
-  event.preventDefault();
-});
